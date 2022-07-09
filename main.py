cbb09697de304968cf19ef0650f17f92aeca4ad4
isonguyom <isonguyom@gmail.com>
2022-07-09 13:56:11 +0100

    set up the project respository

---
+++ main.py
@@ -0,0 +1,5 @@
+# Request the number(1-4) of six-sided dice to be rolled from the user
+num_of_dice = int(input("Please, how many dice do you want to roll (1–4)? "))
+# Accept and parse the user input
+# Roll the dice
+# Print the generated number
