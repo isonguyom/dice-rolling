7693c410f768f3928448fbce7e66c8a288c40a06
isonguyom <isonguyom@gmail.com>
2022-07-10 22:44:21 +0100

    It is done

--- main.py
+++ main.py
@@ -1,39 +1,60 @@
 import random
 
 
+print("WELCOME TO DICE ROLLING SIMULATOR!!!")
 is_running = True
+
+# Roll dice function
+def roll_dice():
+    rolls = []
+    for throw in range(num_of_dice):            
+        # Roll dice
+        roll = random.randint(1, 6) #Dice option is between 1and 6 (1 and 6 included)
+
+        # Create dice appearance
+        if roll == 1:
+            roll = [1]
+        if roll == 2:
+            roll = [2]
+        if roll == 3:
+            roll = [3]
+        if roll == 4:
+            roll = [4]
+        if roll == 5:
+            roll = [5]
+        if roll == 6:
+            roll = [6]
+
+        rolls.append(roll)
+        
+    
+    print(rolls)
 
 # Run loop
 while is_running:
-    # Roll dice
-    roll = random.randint(1, 6) #Dice option is between 1and 6 (1 and 6 included)
-
-    # Print roll result
-    if roll == 1:
-        print("[0]")
-
-    if roll == 2:
-        print("[00]")
+    print("Simulator is running...")
     
-    if roll == 3:
-        print("[000]")
+    # Input number of dice you want roll
+    try:
+        num_list = [1, 2, 3, 4, 5]
+        num_of_dice = int(input("How many dice do you want to roll (maximum of 5)?\n"))
+        if num_of_dice in num_list:
+            roll_dice()
+        else:
+            if num_of_dice > 5:
+                print("Option is more than maximum allowed dice")
+            if num_of_dice < 1:
+                print("Option is less than minimum allowed dice")
+    except:
+        print("Option is not a number")
     
-    if roll == 4:
-        print("[0000]")
-    
-    if roll == 5:
-        print("[00000]")
-    
-    if roll == 6:
-        print("[000000]")
-    
-    
-
     # Check wheather to terminate the loop
     roll_again = input("Would you like to roll again (y/n)? ")
     if roll_again == "y":
         pass 
     
     if roll_again == "n":
+        print("Stopping simulator... ")
+        print("THANKS FOR USING DICE ROLLING SIMULATOR!!!")
         is_running = False 
         # This is the same thing as a break.-      
